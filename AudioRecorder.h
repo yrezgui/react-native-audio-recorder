ee343f5d25d48c7b924578dc5c9f23a41665326c
Yacine Rezgui <rezgui.y@gmail.com>
2015-07-16 19:44:00 +0100

    Add generated files by Xcode

---
+++ AudioRecorder.h
@@ -0,0 +1,13 @@
+//
+//  AudioRecorder.h
+//  RCTText
+//
+//  Created by Yacine Rezgui on 16/07/2015.
+//  Copyright (c) 2015. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AudioRecorder : NSObject
+
+@end
---
+++ AudioRecorder.m
@@ -0,0 +1,13 @@
+//
+//  AudioRecorder.m
+//  RCTText
+//
+//  Created by Yacine Rezgui on 16/07/2015.
+//  Copyright (c) 2015. All rights reserved.
+//
+
+#import "AudioRecorder.h"
+
+@implementation AudioRecorder
+
+@end
